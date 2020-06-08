f46b1f2ffea6bf201ff3be66f5f3d0503a4f50ac
Haujet Zhao <1292756898@qq.com>
2020-06-08 17:39:18 +0800

    Vditor 表情的快捷键是 : ，但是，写进来的时候只能写成 Shift + ; 才行

--- sources/Vditor/生成中间文件.py
+++ sources/Vditor/生成中间文件.py
@@ -3,7 +3,6 @@ import sys
 
 # 运行：python 生成中间文件.py
 # 之后：python utils/export_intermediate_data.py sources/Vditor/intermediate/Vditor.json
-# python utils/export_intermediate_data.py sources/adobe-after-effects/intermediate/after_effects_cc.json
 
 # 将资料库根目录加到 sys.path (这会使 import shmaplib 正常工作)
 CWD = os.path.dirname(os.path.abspath(__file__))
@@ -25,7 +24,7 @@ idata = shmaplib.IntermediateShortcutData(app_name="Vditor", version="2020.6.8",
 # ...添加快捷键到容器，像这样：
 context_name = "通用"
 label = "表情"
-keys_win = ":"
+keys_win = "Shift + ;"
 keys_mac = "Cmd + E"
 idata.add_shortcut(context_name, label, keys_win, keys_mac)
 
